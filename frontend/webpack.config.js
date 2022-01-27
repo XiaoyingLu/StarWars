fb2771831d299c3e8c06c86444a4e5a63d29df4e
Xiaoying Lu <xy415051@dal.ca>
2022-01-26 21:44:30 -0400

    test amplify

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -16,7 +16,6 @@ module.exports = function (_env, argv) {
         output: {
             path: path.resolve(__dirname, "dist"),
             filename: "assets/js/[name].js",
-            publicPath: path.resolve(__dirname, "dist", "./") 
         },
         module: {
             rules: [
